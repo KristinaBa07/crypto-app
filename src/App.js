327c8cc953edb94a97ae81e8a4a575dbe7fd00fc
kristina babanina <kristinababanina0797@gmail.com>
2025-03-04 17:07:28 -0800

    feat:add API methods

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
+import React from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
+    <Router>
+        <div>
+        <NavBar /> 
+        </div>
+    </Router>
+    
   );
 }
 
---
+++ src/services/api.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const API_URL = "https://api.coincap.io/v2/assets";
+
+export const getAssets = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data.data;
+  } catch (error) {
+    console.error("Error fetching assets", error);
+    return [];
+  }
+};
+
+export const getAssetDetail = async (id) => {
+  try {
+    const response = await axios.get(`${API_URL}/${id}`);
+    return response.data.data;
+  } catch (error) {
+    console.error("Error fetching asset details", error);
+    return null;
+  }
+};
+
+export const getAssetHistory = async (id) => {
+  try {
+    const response = await axios.get(`https://api.coincap.io/v2/assets/${id}/history?interval=h1`);
+    return response.data.data.map(item => ({
+      time: new Date(item.time).toLocaleTimeString(),
+      price: parseFloat(item.priceUsd)
+    }));
+  } catch (error) {
+    console.error("Error fetching asset history", error);
+    return [];
+  }
+};
